4519be10f3e045cfcb9d7c3b36f9ead2686ed0d3
Sahim Sakir <sahimsakir@gmail.com>
2022-03-29 16:21:47 +0600

    demo third

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
-'use strict';
-
-
 var windowWidth = window.innerWidth;
-
+var windowWidth2 = 2 * windowWidth;
 
 var horLength = document.querySelector(".element-wrapper").scrollWidth;
 
 var distFromTop = document.querySelector(".horizontal-section").offsetTop;
 
 var scrollDistance = distFromTop + horLength - windowWidth;
-// var scrollHeader = distFromTop + horLength - windowWidth/2;
+
+var hl1 = document.querySelector(".line-one");
+var hl2 = document.querySelector(".line-two");
+var hl3 = document.querySelector(".line-three");
+
 
 document.querySelector(".horizontal-section").style.height = horLength + "px";
 
@@ -20,8 +21,38 @@ window.onscroll = function() {
     if (scrollTop >= distFromTop && scrollTop <= scrollDistance) {
         document.querySelector(".element-wrapper").style.transform = "translateX(-" + (scrollTop - distFromTop) + "px)";
     }
+    if (scrollTop <= windowWidth) {
+        hl1.classList.add('active');
+        hl2.classList.remove('active');
+        hl3.classList.remove('active');
+    } else if (scrollTop >= windowWidth && scrollTop <= windowWidth2) {
+        hl1.classList.remove('active');
+        hl2.classList.add('active');
+        hl3.classList.remove('active');
+    } else {
+        hl1.classList.remove('active');
+        hl2.classList.remove('active');
+        hl3.classList.add('active');
+    }
 
 }
+
+hl1.addEventListener("click", function() {
+    hl1.classList.add('active');
+    hl2.classList.remove('active');
+    hl3.classList.remove('active');
+});
+hl2.addEventListener("click", function() {
+    hl1.classList.remove('active');
+    hl2.classList.add('active');
+    hl3.classList.remove('active');
+});
+hl3.addEventListener("click", function() {
+    hl1.classList.remove('active');
+    hl2.classList.remove('active');
+    hl3.classList.add('active');
+});
+
 var menuText = document.querySelector('.menu-text');
 var navBar = document.querySelector('.navbar');
 var navMenu = document.querySelector('.nav-menu');
